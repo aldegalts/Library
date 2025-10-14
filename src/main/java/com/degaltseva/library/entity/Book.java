c27416dabbc0f7690a2bb1e9f0ac4a13fa974c96
aldegalts <alinadegalceva@mail.ru>
2025-10-14 17:55:03 +0300

    feat: add entities

---
+++ src/main/java/com/degaltseva/library/entity/Book.java
@@ -0,0 +1,61 @@
+package com.degaltseva.library.entity;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
---
+++ src/main/java/com/degaltseva/library/entity/User.java
@@ -0,0 +1,23 @@
+package com.degaltseva.library.entity;
+
+public class User {
+
+    private Long id;
+    private String login;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
