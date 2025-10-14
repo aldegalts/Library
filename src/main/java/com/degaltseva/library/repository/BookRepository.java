97d319afdd1a36d8684245b08f85d8b606b4c2d6
aldegalts <alinadegalceva@mail.ru>
2025-10-14 17:56:29 +0300

    feat: add repositories

---
+++ src/main/java/com/degaltseva/library/repository/BookRepository.java
@@ -0,0 +1,47 @@
+package com.degaltseva.library.repository;
+
+import com.degaltseva.library.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookRepository implements CrudRepository<Book, Long> {
+
+    private final List<Book> bookContainer;
+
+    @Autowired
+    public BookRepository(List<Book> bookContainer) {
+        this.bookContainer = bookContainer;
+    }
+
+    @Override
+    public void create(Book entity) {
+        bookContainer.add(entity);
+    }
+
+    @Override
+    public Book read(Long id) {
+        Optional<Book> book = bookContainer.stream()
+                .filter(b -> b.getId().equals(id))
+                .findFirst();
+        return book.orElse(null);
+    }
+
+    @Override
+    public void update(Book book) {
+        for (int i = 0; i < bookContainer.size(); i++) {
+            if (bookContainer.get(i).getId().equals(book.getId())) {
+                bookContainer.set(i, book);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        bookContainer.removeIf(b -> b.getId().equals(id));
+    }
+}
---
+++ src/main/java/com/degaltseva/library/repository/UserRepository.java
@@ -0,0 +1,46 @@
+package com.degaltseva.library.repository;
+import java.util.List;
+import java.util.Optional;
+
+import com.degaltseva.library.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepository implements CrudRepository<User, Long> {
+
+    private final List<User> userContainer;
+
+    @Autowired
+    public UserRepository(List<User> userContainer) {
+        this.userContainer = userContainer;
+    }
+
+    @Override
+    public void create(User user) {
+        userContainer.add(user);
+    }
+
+    @Override
+    public User read(Long id) {
+        Optional<User> user = userContainer.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst();
+        return user.orElse(null);
+    }
+
+    @Override
+    public void update(User user) {
+        for (int i = 0; i < userContainer.size(); i++) {
+            if (userContainer.get(i).getId().equals(user.getId())) {
+                userContainer.set(i, user);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        userContainer.removeIf(u -> u.getId().equals(id));
+    }
+}
